31f5e420f9588bbc9e34e88e08127f42be9a64a8
Otavio F <otaviofbrito@gmail.com>
2024-09-21 17:52:55 -0300

    en ew calculator

--- template/script.js
+++ template/script.js
@@ -1,12 +1,10 @@
 function calcularOcupacao() {
   // Obtendo os valores dos inputs
-  const taxaEntrada = parseFloat(document.getElementById('entrada').value)
-  const taxaAtendimento = parseFloat(
-    document.getElementById('atendimento').value
-  )
+  const taxaEntrada = parseFloat(document.getElementById('util_lambda').value)
+  const taxaAtendimento = parseFloat(document.getElementById('util_mu').value)
   // Verificando se todos os valores foram inseridos
   if (isNaN(taxaEntrada) || isNaN(taxaAtendimento)) {
-    document.getElementById('resultado').innerText = ' = Not valid!'
+    document.getElementById('util_res').innerText = ' = Not valid!'
     return
   }
 
@@ -14,5 +12,15 @@ function calcularOcupacao() {
   const ocupacaoCalculada = ((taxaAtendimento / taxaEntrada) * 100).toFixed(2)
 
   // Exibindo o resultado na página
-  document.getElementById('resultado').innerText = ` = ${ocupacaoCalculada}%`
+  document.getElementById('util_res').innerText = ` = ${ocupacaoCalculada}%`
+}
+
+function calcularL() {
+  const rho = parseFloat(document.getElementById('e_rho').value)
+  if (isNaN(rho)) {
+    document.getElementById('e_res').innerText = ' = Not valid!'
+    return
+  }
+  const e_n = (rho / (1 - rho)).toFixed(2)
+  document.getElementById('e_res').innerText = ` = ${e_n}`
 }
