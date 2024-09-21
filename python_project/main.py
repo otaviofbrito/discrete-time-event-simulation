303aed441eaf5e0446b62669cd9265ca8a412f5f
Otavio F <otaviofbrito@gmail.com>
2024-09-21 01:54:12 -0300

    fix main.py input

--- python_project/main.py
+++ python_project/main.py
@@ -19,15 +19,43 @@ def open_report():
 
 
 def main():
-    num_simulations = int(input("Número de simulações: "))
+    print()
+    print("+" + "-" * 58 + "+")
+    print("|" + " " * 12 + ">>Simulador de Eventos Discretos<<" + " " * 12 + "|")
+    print("+" + "-" * 58 + "+\n")
+
+    while True:
+        try:
+            num_simulations = int(input("Número de simulações: "))
+            if num_simulations <= 0:
+                raise ValueError(
+                    "O número de simulações deve ser maior que zero.")
+            break  # Se a entrada for válida, saia do loop
+        except ValueError as e:
+            print(f"Entrada inválida: {e}. Tente novamente.")
 
     for i in range(num_simulations):
-        seed = input(f"\nSeed aleatória[{i+1}]: ")
-        tx_chegada = input(f"Taxa média de chegada[{i+1}]: ")
-        tx_saida = input(f"Taxa média de atendimento[{i+1}]: ")
-        tempo = input(f"Tempo de simulação[{i+1}]: ")
-        run_simulation(int(seed), float(tx_chegada),
-                       float(tx_saida), int(tempo))
+        while True:
+            try:
+                seed = input(f"\nSeed aleatória[{i+1}]: ")
+                if not seed.isdigit():
+                    raise ValueError("A seed deve ser um número inteiro.")
+                tx_chegada = float(input(f"Taxa média de chegada[{i+1}]: "))
+                if tx_chegada < 0:
+                    raise ValueError(
+                        "A taxa média de chegada deve ser não negativa.")
+                tx_saida = float(input(f"Taxa média de atendimento[{i+1}]: "))
+                if tx_saida < 0:
+                    raise ValueError(
+                        "A taxa média de atendimento deve ser não negativa.")
+                tempo = int(input(f"Tempo de simulação[{i+1}]: "))
+                if tempo <= 0:
+                    raise ValueError(
+                        "O tempo de simulação deve ser maior que zero.")
+                run_simulation(int(seed), tx_chegada, tx_saida, tempo)
+                break
+            except ValueError as e:
+                print(f"Entrada inválida: {e}. Tente novamente.")
 
     generate_report()
     open_report()
